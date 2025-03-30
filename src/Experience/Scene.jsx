81b5a2789a78d893ecfa04938b6e50879ba7fce0
Andrew Woan <andrewwoan@gmail.com>
2025-03-30 08:34:28 -0400

    optimziation

--- src/Experience/Scene.jsx
+++ src/Experience/Scene.jsx
@@ -72,6 +72,11 @@ const Scene = ({
     new THREE.Euler().copy(rotationTargets[0].rotation)
   );
   const timeRef = useRef(0);
+  const [shouldRenderBackground, setShouldRenderBackground] = useState(true);
+
+  useEffect(() => {
+    setShouldRenderBackground(window.innerWidth >= 764);
+  }, []);
 
   const { shouldRenderChunk } = useChunkedLoading();
 
@@ -211,7 +216,7 @@ const Scene = ({
       <Suspense fallback={null}>
         <First />
         <Second />
-        <Background />
+        {shouldRenderBackground && <Background />}
       </Suspense>
 
       {shouldRenderChunk(1) && (
